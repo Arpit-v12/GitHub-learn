c1e3fdd1c2e429397e6ebb6acf6f7066fd69a031
Arpit-v12 <vimalarpit201@gmail.com>
2020-07-20 12:01:33 +0530

    for loops efficirncy program

---
+++ for_loops.py
@@ -0,0 +1,22 @@
+for i in range(10):
+	print('Done')
+
+"""
+For algorithm time
+"""
+import time
+n=100
+start=time.time()
+sum=0
+sum_1=0
+for i in range(1):
+	sum+=i
+print(sum+0.00)
+end=time.time()
+print('Time-Taken:, ned-start')
+
+start1=time.time()
+sum_1=(n*(n-1)/2)
+print(sum_1)
+end1=time.time()
+print('Time-Taken:,end1-start1')
