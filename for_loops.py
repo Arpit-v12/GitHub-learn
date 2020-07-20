f7a2dc498af74e98a36871e7d84d773189ff3ef7
Arpit <68511836+Arpit-v12@users.noreply.github.com>
2020-07-20 21:44:56 +0530

    Error fixed

--- for_loops.py
+++ for_loops.py
@@ -11,12 +11,12 @@ sum=0
 sum_1=0
 for i in range(1):
 	sum+=i
-print(sum+0.00)
+print(sum)
 end=time.time()
-print('Time-Taken:,'end-start)
+print('Time-Taken: ', end-start)
 
 start1=time.time()
 sum_1=(n*(n-1)/2)
 print(sum_1)
 end1=time.time()
-print('Time-Taken:,'end1-start1)
+print('Time-Taken:',end1-start1)
